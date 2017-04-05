0bc4757af5c4f8e231bdf534d38a345520e1f5a5
luhaiming <luhaiming@zillionfortune.com>
2017-04-05 16:52:49 +0800

    add all file

--- src/main/java/com/jackie/smartapi/controller/HomeController.java
+++ src/main/java/com/jackie/smartapi/controller/HomeController.java
@@ -1,25 +1,24 @@
 package com.jackie.smartapi.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class HomeController {
-
-    @RequestMapping(value = "/")
-    public ModelAndView test(HttpServletResponse response) throws IOException {
-        return new ModelAndView("home");
-    }
 
     @ResponseBody
     @RequestMapping(value = "/test")
     public String test1(HttpServletResponse response) throws IOException {
         return "test";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/test1")
+    public String test2(HttpServletResponse response) throws IOException {
+        return "test1";
     }
 }
--- src/main/java/com/jackie/smartapi/controller/ProjectController.java
+++ src/main/java/com/jackie/smartapi/controller/ProjectController.java
@@ -1,32 +1,52 @@
 package com.jackie.smartapi.controller;
 
+import com.jackie.smartapi.Model.Project;
 import com.jackie.smartapi.dao.ProjectDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
 
 /**
  * Created by luhaiming on 2017/3/23 0023.
  */
 @Controller
-@RequestMapping("/project")
 public class ProjectController {
 
+    @Autowired
     public ProjectDAO projectDAO;
 
     @ResponseBody
-    @RequestMapping("/create")
-    public String insertProject(HttpServletRequest request, HttpServletResponse response) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
-        projectDAO = (ProjectDAO) ctx.getBean("stockDAO");
-
+    @RequestMapping(value = "/create", method = RequestMethod.POST, headers = "Content-Type=application/json")
+    public String insertProject(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        InputStream is = request.getInputStream();
+        byte bytes[] = new byte[request.getContentLength()];
+        is.read(bytes);
+        String jsonStr = new String(bytes, request.getCharacterEncoding());
+        projectDAO.createProject(new Project());
         return "create success";
-
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/getall", method = RequestMethod.GET)
+    public String getAll() {
+
+        return "create success";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    public String getProject() {
+        return "create success";
+    }
 }
+
