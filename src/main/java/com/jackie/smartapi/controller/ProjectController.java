1ea0c09de5b5fcfda1f2ad432d6e9840a74d35fe
luhaiming <luhaiming@zillionfortune.com>
2017-04-20 16:24:52 +0800

    add all my file

--- src/main/java/com/jackie/smartapi/controller/ProjectController.java
+++ src/main/java/com/jackie/smartapi/controller/ProjectController.java
@@ -57,11 +57,11 @@ public class ProjectController {
     @RequestMapping(value = "/getall", method = RequestMethod.GET)
     public String getAll(ServletResponse res) {
 
-        HttpServletResponse response = (HttpServletResponse) res;
+     /*   HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");*/
 
         ProjectDAO projectDAO = (ProjectDAO) ctx.getBean("projectDAO");
         List<Map<String, Object>> resList = projectDAO.getAllProject();
